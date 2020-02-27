687cb910ea8eb782453c103b768dbb704c3bbfc0
Henrique Mangueira <henriquesantoszero@gmail.com>
2020-02-27 16:12:41 -0300

    primeiro commit

---
+++ AquiExpress/src/main/java/com/aquiexpress/AquiExpressApplication.java
@@ -0,0 +1,13 @@
+package com.aquiexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquiExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquiExpressApplication.class, args);
+	}
+
+}
---
+++ AquiExpress/src/test/java/com/aquiexpress/AquiExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aquiexpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquiExpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
